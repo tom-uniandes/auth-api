f1dbb762472e0ceef6baf821e93e8f18d6e0402e
Tomas Rodriguez <t.rodriguezh@uniandes.edu.co>
2024-10-20 12:20:57 -0500

    Add manage exceptions errors

--- src/clients/manage_client.py
+++ src/clients/manage_client.py
@@ -4,14 +4,15 @@ from src.errors.errors import Unavailable, Bad_Request
 
 class ManageClient:
     def __init__(self):
-        self.gateway_path = os.environ.get("GATEWAY_PATH")
-        #self.gateway_path = "http://localhost:4999"
+        self.gateway_path = "http://localhost:5000"
+        if os.environ.get("GATEWAY_PATH"):
+            self.gateway_path = os.environ.get("GATEWAY_PATH")
         self.manage_client_url = f'{self.gateway_path}/clients/create_client'
         self.headers = {"Content-Type": "application/json"}
 
-    def register_client(self, user_uid):
+    def register_client(self, data):
         try:
-            response = requests.post(self.gateway_path, json={"user_id":user_uid}, headers=self.headers)
+            response = requests.post(self.manage_client_url, json=data, headers=self.headers)
         
             if response.status_code == 400:
                 raise Bad_Request(response.json().get("msg", "Error en los campos, por favor revise la informacion ingresada"))
